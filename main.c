834e34047e6477996202326e712a3197b908d4fe
Villagerjj <56765269+Villagerjj@users.noreply.github.com>
2022-11-24 12:19:26 -0500

    Add files via upload

--- main.c
+++ main.c
@@ -7,6 +7,7 @@
 #include <gb/cgb.h>
 #include "graphics.h"
 #include "Species.h"
+#include "BackgroundData.h"
 #define MAP_WIDTH 32
 #define MAP_HEIGHT 32
 #define PLANT_MAX 50
@@ -14,9 +15,18 @@
 #define ANIMAL_MAX 100
 #define NUMBER_OF_SPECIES 2
 #define ANIMAL_START 30
-struct animal wolves[ANIMAL_MAX/NUMBER_OF_SPECIES];
-struct animal goats[ANIMAL_MAX/NUMBER_OF_SPECIES];
+struct animal wolves[ANIMAL_MAX / NUMBER_OF_SPECIES];
+struct animal goats[ANIMAL_MAX / NUMBER_OF_SPECIES];
 struct plant trees[PLANT_MAX];
+
+
+const UWORD backgroundpalette[] = 
+{
+    BackgroundDataCGBPal0c0,BackgroundDataCGBPal0c1,BackgroundDataCGBPal0c2,BackgroundDataCGBPal0c3,
+    BackgroundDataCGBPal1c0,BackgroundDataCGBPal1c1,BackgroundDataCGBPal1c2,BackgroundDataCGBPal1c3,
+    BackgroundDataCGBPal2c0,BackgroundDataCGBPal2c1,BackgroundDataCGBPal2c2,BackgroundDataCGBPal2c3,
+    BackgroundDataCGBPal3c0,BackgroundDataCGBPal3c1,BackgroundDataCGBPal3c2,BackgroundDataCGBPal3c3,
+};
 
 typedef struct animal
 {
@@ -28,9 +38,10 @@ typedef struct animal
     uint8_t species;    // what smelly species ar dey?
     uint8_t standingOn; // just the tile id
     BOOLEAN Dead;       // 1 for dead
+    uint8_t gotoID; //the id of the thing ur going two
     uint8_t gotox;      // the cords the creature will try and navigate to.
     uint8_t gotoy;
-    uint8_t avoidx; //the cords of the nearest threat
+    uint8_t avoidx; // the cords of the nearest threat
     uint8_t avoidy;
     uint8_t x; // tile cord of thing
     uint8_t y;
@@ -56,122 +67,120 @@ uint8_t randomInRange(uint8_t lower, uint8_t upper)
 
 void closestPlant(uint8_t ID, uint8_t ENTITY_TYPE, uint8_t Species) // 0 for animal, 1 for plant
 {
-    
-if (ENTITY_TYPE == 0)
-{
-    for (uint8_t p; p < PLANT_MAX; p++)
+
+    if (ENTITY_TYPE == 0)
     {
-        if(trees[p].x - goats[ID].x <= 20)
+        for (uint8_t p; p < PLANT_MAX; p++)
         {
-            if(trees[p].y - goats[ID].y <=20)
+            if (trees[p].x - goats[ID].x <= 20)
             {
-                goats[ID].gotox = trees[p].x;
-                goats[ID].gotoy = trees[p].y;
+                if (trees[p].y - goats[ID].y <= 20)
+                {
+                    goats[ID].gotox = trees[p].x;
+                    goats[ID].gotoy = trees[p].y;
+                }
             }
         }
     }
-}
-else
-{
-    for (uint8_t p; p < PLANT_MAX; p++)
+    else
     {
-        if(trees[p].x - trees[ID].x <= 30)
+        for (uint8_t p; p < PLANT_MAX; p++)
         {
-            if(trees[p].y - trees[ID].y <=30)
+            if (trees[p].x - trees[ID].x <= 30)
             {
-                trees[ID].isNearWater == 1;
+                if (trees[p].y - trees[ID].y <= 30)
+                {
+                    trees[ID].isNearWater == 1;
+                }
             }
         }
     }
-}
 }
 
 void closestAnimal(uint8_t ID, uint8_t ENTITY_TYPE, uint8_t Species)
 {
-if (ENTITY_TYPE == 0)
-{
-    switch (Species)
+    if (ENTITY_TYPE == 0)
     {
-    case GOATS:
-        /* code */
-        break;
-    
-    case WOLVES:
-        for (uint8_t p; p < ANIMAL_MAX/NUMBER_OF_SPECIES; p++)
+        switch (Species)
         {
-        if(goats[p].x - wolves[ID].x <= 20)
-        {
-            if(goats[p].y - wolves[ID].y <=20)
-            {
-                wolves[ID].gotox = goats[p].x;
-                wolves[ID].gotoy = goats[p].y;
-            }
-        }
-        }
-        break;
+        case GOATS:
+            /* code */
+            break;
 
-    default:
-        break;
-    }
-    
-}
-else
-{
-    for (uint8_t p; p < PLANT_MAX; p++)
-    {
-        if(trees[p].x - trees[ID].x <= 30)
-        {
-            if(trees[p].y - trees[ID].y <=30)
+        case WOLVES:
+            for (uint8_t p; p < ANIMAL_MAX / NUMBER_OF_SPECIES; p++)
             {
-                trees[ID].isNearWater == 1;
+                if (goats[p].x - wolves[ID].x <= 20)
+                {
+                    if (goats[p].y - wolves[ID].y <= 20)
+                    {
+                        wolves[ID].gotox = goats[p].x;
+                        wolves[ID].gotoy = goats[p].y;
+                    }
+                }
+            }
+            break;
+
+        default:
+            break;
+        }
+    }
+    else
+    {
+        for (uint8_t p; p < PLANT_MAX; p++)
+        {
+            if (trees[p].x - trees[ID].x <= 30)
+            {
+                if (trees[p].y - trees[ID].y <= 30)
+                {
+                    trees[ID].isNearWater == 1;
+                }
             }
         }
     }
-}
 }
 
 void avoidClosestAnimal(uint8_t ID, uint8_t ENTITY_TYPE, uint8_t Species)
 {
-if (ENTITY_TYPE == 0)
-{
-    switch (Species)
+    if (ENTITY_TYPE == 0)
     {
-    case GOATS:
-        for (uint8_t p; p < ANIMAL_MAX/NUMBER_OF_SPECIES; p++)
+        switch (Species)
         {
-        if(wolves[p].x - goats[ID].x <= 20)
-        {
-            if(wolves[p].y - goats[ID].y <=20)
+        case GOATS:
+            for (uint8_t p; p < ANIMAL_MAX / NUMBER_OF_SPECIES; p++)
             {
-                goats[ID].avoidx = wolves[p].x;
-                goats[ID].avoidy = wolves[p].y;
+                if (wolves[p].x - goats[ID].x <= 20)
+                {
+                    if (wolves[p].y - goats[ID].y <= 20)
+                    {
+                        goats[ID].avoidx = wolves[p].x;
+                        goats[ID].avoidy = wolves[p].y;
+                    }
+                }
             }
-        }
-        }
-        break;
-    
-    case WOLVES:
-        
-        break;
+            break;
 
-    default:
-        break;
+        case WOLVES:
+
+            break;
+
+        default:
+            break;
+        }
     }
-    
-}
-else
-{
-    for (uint8_t p; p < PLANT_MAX; p++)
+    else
     {
-        if(trees[p].x - trees[ID].x <= 30)
+        for (uint8_t p; p < PLANT_MAX; p++)
         {
-            if(trees[p].y - trees[ID].y <=30)
+            if (trees[p].x - trees[ID].x <= 30)
             {
-                trees[ID].isNearWater == 1;
+                if (trees[p].y - trees[ID].y <= 30)
+                {
+                    trees[ID].isNearWater == 1;
+                }
             }
         }
     }
-}
 }
 
 void closestWater(uint8_t ID, uint8_t ENTITY_TYPE, uint8_t Species) // 0 for animal, 1 for plant
@@ -197,7 +206,6 @@ void closestWater(uint8_t ID, uint8_t ENTITY_TYPE, uint8_t Species) // 0 for ani
                     default:
                         break;
                     }
-                    
                 }
                 else
                 {
@@ -211,7 +219,7 @@ void closestWater(uint8_t ID, uint8_t ENTITY_TYPE, uint8_t Species) // 0 for ani
 
 void animalTick()
 {
-    for (uint8_t a; a < ANIMAL_MAX/NUMBER_OF_SPECIES; a++) //wolves
+    for (uint8_t a; a < ANIMAL_MAX / NUMBER_OF_SPECIES; a++) // wolves
     {
         if (wolves[a].Dead == 0)
         {
@@ -234,13 +242,13 @@ void animalTick()
                 wolves[a].target = 1; // target is water
                 closestWater(a, 0, WOLVES);
             }
-/*
-            if (wolves[a].thirst >= 60 && wolves[a].hunger >= 60)
-            {
-                wolves[a].target = 0; // target is to chill
-               // wolves[a].gotox = 20;
-               // wolves[a].gotoy = 18;
-            }*/
+            /*
+                        if (wolves[a].thirst >= 60 && wolves[a].hunger >= 60)
+                        {
+                            wolves[a].target = 0; // target is to chill
+                           // wolves[a].gotox = 20;
+                           // wolves[a].gotoy = 18;
+                        }*/
 
             if (wolves[a].hunger <= 0)
             {
@@ -251,12 +259,12 @@ void animalTick()
                 wolves[a].hunger--;
             }
 
-            if(wolves[a].target != 0)
+            if (wolves[a].target != 0)
             {
                 wolves[a].energy--;
             }
 
-            if(wolves[a].energy == 0)
+            if (wolves[a].energy == 0)
             {
                 wolves[a].Dead = 1;
             }
@@ -274,7 +282,7 @@ void animalTick()
                 wolves[a].x = 0;
                 wolves[a].standingOn = get_bkg_tile_xy(wolves[a].x, wolves[a].y);
             }
-            else if ( wolves[a].y >= MAP_HEIGHT)
+            else if (wolves[a].y >= MAP_HEIGHT)
             {
                 set_bkg_tile_xy(wolves[a].x, wolves[a].y, wolves[a].standingOn);
                 wolves[a].y = 0;
@@ -359,7 +367,7 @@ void animalTick()
         }
     }
 
-    for (uint8_t a; a < ANIMAL_MAX/NUMBER_OF_SPECIES; a++) //goats
+    for (uint8_t a; a < ANIMAL_MAX / NUMBER_OF_SPECIES; a++) // goats
     {
         if (goats[a].Dead == 0)
         {
@@ -383,13 +391,13 @@ void animalTick()
                 goats[a].target = 1; // target is water
                 closestWater(a, 0, GOATS);
             }
-/*
-            if (goats[a].thirst >= 60 && goats[a].hunger >= 60)
-            {
-                goats[a].target = 0; // target is to chill
-               // goats[a].gotox = 20;
-               // goats[a].gotoy = 18;
-            }*/
+            /*
+                        if (goats[a].thirst >= 60 && goats[a].hunger >= 60)
+                        {
+                            goats[a].target = 0; // target is to chill
+                           // goats[a].gotox = 20;
+                           // goats[a].gotoy = 18;
+                        }*/
 
             if (goats[a].hunger <= 0)
             {
@@ -400,12 +408,12 @@ void animalTick()
                 goats[a].hunger--;
             }
 
-            if(goats[a].target != 0)
+            if (goats[a].target != 0)
             {
                 goats[a].energy--;
             }
 
-            if(goats[a].energy == 0)
+            if (goats[a].energy == 0)
             {
                 goats[a].Dead = 1;
             }
@@ -423,7 +431,7 @@ void animalTick()
                 goats[a].x = 0;
                 goats[a].standingOn = get_bkg_tile_xy(goats[a].x, goats[a].y);
             }
-            else if ( goats[a].y >= MAP_HEIGHT)
+            else if (goats[a].y >= MAP_HEIGHT)
             {
                 set_bkg_tile_xy(goats[a].x, goats[a].y, goats[a].standingOn);
                 goats[a].y = 0;
@@ -506,29 +514,29 @@ void animalTick()
             }
 
             if (goats[a].x < goats[a].avoidx)
-                {
-                    set_bkg_tile_xy(goats[a].x, goats[a].y, goats[a].standingOn);
-                    goats[a].x--;
-                    goats[a].standingOn = get_bkg_tile_xy(goats[a].x, goats[a].y);
-                }
-                if (goats[a].x > goats[a].avoidx)
-                {
-                    set_bkg_tile_xy(goats[a].x, goats[a].y, goats[a].standingOn);
-                    goats[a].x++;
-                    goats[a].standingOn = get_bkg_tile_xy(goats[a].x, goats[a].y);
-                }
-                if (goats[a].y > goats[a].avoidy)
-                {
-                    set_bkg_tile_xy(goats[a].x, goats[a].y, goats[a].standingOn);
-                    goats[a].y++;
-                    goats[a].standingOn = get_bkg_tile_xy(goats[a].x, goats[a].y);
-                }
-                if (goats[a].y < goats[a].avoidy)
-                {
-                    set_bkg_tile_xy(goats[a].x, goats[a].y, goats[a].standingOn);
-                    goats[a].y--;
-                    goats[a].standingOn = get_bkg_tile_xy(goats[a].x, goats[a].y);
-                }
+            {
+                set_bkg_tile_xy(goats[a].x, goats[a].y, goats[a].standingOn);
+                goats[a].x--;
+                goats[a].standingOn = get_bkg_tile_xy(goats[a].x, goats[a].y);
+            }
+            if (goats[a].x > goats[a].avoidx)
+            {
+                set_bkg_tile_xy(goats[a].x, goats[a].y, goats[a].standingOn);
+                goats[a].x++;
+                goats[a].standingOn = get_bkg_tile_xy(goats[a].x, goats[a].y);
+            }
+            if (goats[a].y > goats[a].avoidy)
+            {
+                set_bkg_tile_xy(goats[a].x, goats[a].y, goats[a].standingOn);
+                goats[a].y++;
+                goats[a].standingOn = get_bkg_tile_xy(goats[a].x, goats[a].y);
+            }
+            if (goats[a].y < goats[a].avoidy)
+            {
+                set_bkg_tile_xy(goats[a].x, goats[a].y, goats[a].standingOn);
+                goats[a].y--;
+                goats[a].standingOn = get_bkg_tile_xy(goats[a].x, goats[a].y);
+            }
         }
     }
     /*
@@ -549,7 +557,7 @@ void display()
     // world view updater
     // printf("Hello World!");
     animalTick();
-    for (uint8_t a; a < ANIMAL_MAX/NUMBER_OF_SPECIES; a++) //goats
+    for (uint8_t a; a < ANIMAL_MAX / NUMBER_OF_SPECIES; a++) // goats
     {
 
         if (wolves[a].Dead == 1)
@@ -562,7 +570,7 @@ void display()
         }
     }
 
-    for (uint8_t a; a < ANIMAL_MAX/NUMBER_OF_SPECIES; a++) //goats
+    for (uint8_t a; a < ANIMAL_MAX / NUMBER_OF_SPECIES; a++) // goats
     {
 
         if (goats[a].Dead == 1)
@@ -575,10 +583,7 @@ void display()
         }
     }
 
-    for (uint8_t p; p < PLANT_MAX; p++)
-    {
-        set_bkg_tile_xy(trees[p].x, trees[p].y, 4);
-    }
+    
 }
 
 void genRiverX()
@@ -642,15 +647,13 @@ void genRiverY()
     set_bkg_tile_xy(X_POS + noise[0] + 2, 0, 2);
 }
 
-
-
 void initSim()
 {
     initarand(sys_time);
     clearScreen();
     genRiverX();
     genRiverY();
-    for (uint8_t a; a < ANIMAL_START/NUMBER_OF_SPECIES; a++)
+    for (uint8_t a; a < ANIMAL_START / NUMBER_OF_SPECIES; a++)
     {
         goats[a].age = 1;
         goats[a].thirst = 100;
@@ -669,7 +672,7 @@ void initSim()
         }
     }
 
-    for (uint8_t a; a < ANIMAL_START/NUMBER_OF_SPECIES; a++)
+    for (uint8_t a; a < ANIMAL_START / NUMBER_OF_SPECIES; a++)
     {
         wolves[a].age = 1;
         wolves[a].thirst = 100;
@@ -703,6 +706,11 @@ void initSim()
             trees[p].y = randomInRange(0, MAP_HEIGHT);
         }
     }
+
+    for (uint8_t p; p < PLANT_MAX; p++)
+    {
+        set_bkg_tile_xy(trees[p].x, trees[p].y, 4);
+    }
 }
 
 inline void set_bkg_tile_attrib_xy(uint8_t x, uint8_t y, uint8_t attrib_data)
@@ -721,6 +729,7 @@ void cgb_check_and_init(void)
     if (_cpu == CGB_TYPE)
     {
         cpu_fast();
+        set_bkg_palette(0, 4, &backgroundpalette[0]); 
         // maybe set up your cgb palettes
     }
 }
@@ -754,20 +763,20 @@ void main(void)
 
         if (cur & J_UP)
         {
-            scroll_bkg(0,-8);
+            scroll_bkg(0, -8);
         }
         else if (cur & J_DOWN)
         {
-            scroll_bkg(0,8);
+            scroll_bkg(0, 8);
         }
 
         if (cur & J_LEFT)
         {
-            scroll_bkg(-8,0);
+            scroll_bkg(-8, 0);
         }
         else if (cur & J_RIGHT)
         {
-            scroll_bkg(8,0);
+            scroll_bkg(8, 0);
         }
 
         display();
